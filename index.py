dae1901aed862fa27e67ba7061a90840300b549b
Ramamohan <mohanarama0055@gmail.com>
2025-02-12 22:53:01 +0530

    Test

--- index.py
+++ index.py
@@ -1,23 +1,32 @@
 import streamlit as st
+import yfinance as yf
 
 st.set_page_config("Ram Mohan", layout="wide")
 st.title("stock market")
-stockname=["ZOMATO.NS"]
-st.selectbox(label="selectstock", options= stockname)
-download=st.button("DOWNLOAD")
+stockname=["ZOMATO.NS","INFY.NS"]
+col1,col2=st.columns([2,2])
+with col1:
+    data=st.selectbox(label="selectstock", options= stockname,label_visibility="collapsed")
+with col2:
+    download=st.button("DOWNLOAD")
 if download:
-    import yfinance as yf
-    stock = yf.Ticker("ZOMATO.NS")
-    df = stock.history(period="1mo", interval="1d")
+    stock = yf.Ticker(data)
+    stockname=stock.info.get('shortName')
+
+    df = stock.history(period="1mo")
     df = df.reset_index()
-    st.write(df.head())
+    st.header(stockname)
+    st.write(df.tail())
     st.write(df.columns)
-    col1, col2 = st.columns([1,2])
+    col1, col2 = st.columns([1,1])
     with col1:
-        st.line_chart(df, x="Date", y="Close")
+        st.line_chart(df, x="Date", y="Close",color="#ff00f0")
+        st.area_chart(df,x="Date", y="Volume",color="#ff00ff")
         st.divider()
     with col2:
         st.bar_chart(df, x="Date", y="Volume")
+        st.scatter_chart(df, x="Date",y="High",color=["#FF0000"])
+
 
 
 
