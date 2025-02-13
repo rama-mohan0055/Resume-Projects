123a9d87cb0be1e2f8eee52da3139222a8756247
Ramamohan <mohanarama0055@gmail.com>
2025-02-13 19:04:23 +0530

    Test

--- index.py
+++ index.py
@@ -1,31 +1,42 @@
 import streamlit as st
 import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df=pd.read_csv("filtered_nifty_50.csv")
 
 st.set_page_config("Ram Mohan", layout="wide")
 st.title("stock market")
-stockname=["ZOMATO.NS","INFY.NS"]
+stocknames=df['NAME OF COMPANY'].to_list()
 col1,col2=st.columns([2,2])
 with col1:
-    data=st.selectbox(label="selectstock", options= stockname,label_visibility="collapsed")
+    data=st.selectbox(label="selectstock", options= stocknames,label_visibility="collapsed")
 with col2:
     download=st.button("DOWNLOAD")
 if download:
-    stock = yf.Ticker(data)
+    symbol = df.loc[df["NAME OF COMPANY"]==data,'SYMBOL'].values[0]+".NS"
+    stock = yf.Ticker(symbol)
     stockname=stock.info.get('shortName')
 
     df = stock.history(period="1mo")
     df = df.reset_index()
     st.header(stockname)
+    df.drop(["Dividends","Stock Splits"],axis=1,inplace=True)
     st.write(df.tail())
     st.write(df.columns)
     col1, col2 = st.columns([1,1])
     with col1:
         st.line_chart(df, x="Date", y="Close",color="#ff00f0")
         st.area_chart(df,x="Date", y="Volume",color="#ff00ff")
+        st.bar_chart(df, x="Date", y="Close",color="#00ff00")
+
         st.divider()
     with col2:
         st.bar_chart(df, x="Date", y="Volume")
         st.scatter_chart(df, x="Date",y="High",color=["#FF0000"])
+        fig, ax = plt.subplots(figsize=(10,5))
+        ax.hist(df["Volume"])
+        st.pyplot(fig)
 
 
 
