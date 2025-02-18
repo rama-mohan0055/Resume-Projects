dfcec79f02698990937c77b2a129465d1ef9437e
Ramamohan <mohanarama0055@gmail.com>
2025-02-18 18:35:05 +0530

    test

--- index.py
+++ index.py
@@ -4,45 +4,76 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import datetime
+import datetime as dt
 
 df=pd.read_csv("filtered_nifty_50.csv")
 
 st.set_page_config("Ram Mohan", layout="wide")
 st.title("stock market")
+
+st.sidebar.title("Navigation")
+selected_option = st.sidebar.radio("Select an option:", ["Summary", "News", "Chart"])
+
 stocknames=df['NAME OF COMPANY'].to_list()
 col1,col2=st.columns([2,2])
 with col1:
     data=st.selectbox(label="selectstock", options= stocknames,label_visibility="collapsed")
 with col2:
     download=st.button("DOWNLOAD")
-if download:
-    symbol = df.loc[df["NAME OF COMPANY"]==data,'SYMBOL'].values[0]+".NS"
-    stock = yf.Ticker(symbol)
-    stockname=stock.info.get('shortName')
 
-    df = stock.history(period="1mo")
-    df = df.reset_index()
-    st.header(stockname)
+symbol = df.loc[df["NAME OF COMPANY"]==data,'SYMBOL'].values[0]+".NS"
+stock = yf.Ticker(symbol)
 
-    df.drop(["Dividends","Stock Splits"],axis=1,inplace=True)
-    st.write(df.tail())
-    st.write(df.columns)
-    col1, col2 = st.columns([1,1])
+if selected_option=="Summary":
+    st.header("Summary")
+    st.write(stock.info['address1'])
+    st.write(stock.info['city'])
 
-    with col1:
+elif selected_option=="News":
+    st.header("News")
+    news = stock.news
+    #st.write(news)
+
+    for article in news:
+        data=article['content']
+        #st.write(data.keys())
+
+        title = data['title']
+        st.subheader(title)
+        with st.expander("See explanation"):
+            st.write(data["summary"]
+            )
+
+
+else:
+    st.header("charts")   
+    
+    if download:
+       
+        stockname=stock.info.get('shortName',)
         
-        d = st.date_input("Date Wise")
-        st.write("Date Wise:", d)
-        st.line_chart(df, x="Date", y="Close",color="#ff00f0")
-        st.area_chart(df,x="Date", y="Volume",color="#ff00ff")
-        st.divider()
-    with col2:
-        st.bar_chart(df, x="Date", y="Volume")
-        st.scatter_chart(df, x="Date",y="High",color=["#FF0000"])
-        fig = px.bar(df, x='Date', y='Volume')
-        st.plotly_chart(fig)       
-        
+
+        df = stock.history(period="1mo")
+        df = df.reset_index()
+        st.header(stockname)
+
+        df.drop(["Dividends","Stock Splits"],axis=1,inplace=True)
+        st.write(df.tail())
+        st.write(df.columns)
+        col1, col2 = st.columns([1,1])
+
+        with col1:
+            
+            
+            st.line_chart(df, x="Date", y="Close",color="#ff00f0")
+            st.area_chart(df,x="Date", y="Volume",color="#ff00ff")
+            st.divider()
+        with col2:
+            st.bar_chart(df, x="Date", y="Volume")
+            st.scatter_chart(df, x="Date",y="High",color=["#FF0000"])
+            fig = px.bar(df, x='Date', y='Volume')
+            st.plotly_chart(fig)       
+            
 
 
 
--- testing.py
+++ testing.py
@@ -1,56 +1,25 @@
-import streamlit as st
-import yfinance as yf
-import pandas as pd
-import plotly.express as px
-import datetime
+timeframes = {
+"1D": "1d",
+"5D": "5d",
+"1M": "1mo",
+"6M": "6mo",
+"YTD": f"{dt.datetime.now().year}-01-01",
+"1Y": "1y",
+"5Y": "5y",
+"All": None}
 
-# Read Data
-df = pd.read_csv("filtered_nifty_50.csv")
+selected_timeframe = st.selectbox("Select Timeframe", list(timeframes.keys()))
+if selected_timeframe:
+    if selected_timeframe == "All":
+        data = yf.download(stocknames)
+    else:
+        period = timeframes[selected_timeframe]
+        if period == "1d":
+            data = yf.download(stocknames, period=period, interval="1m")
+        else:
+            data = yf.download(stocknames, period=period)
 
-# Page Config
-st.set_page_config("Ram Mohan", layout="wide")
-st.title("Stock Market")
+        st.subheader(f"Data for {stocknames} over {selected_timeframe}")
+        st.line_chart(data['Close'])
 
-# Stock Selection
-stocknames = df['NAME OF COMPANY'].to_list()
-col1, col2 = st.columns([2, 2])
-
-with col1:
-    data = st.selectbox(label="Select Stock", options=stocknames, label_visibility="collapsed")
-
-with col2:
-    download = st.button("DOWNLOAD")
-
-if download:
-    symbol = df.loc[df["NAME OF COMPANY"] == data, 'SYMBOL'].values[0] + ".NS"
-    stock = yf.Ticker(symbol)
-    stockname = stock.info.get('shortName')
-
-    df = stock.history(period="1mo").reset_index()
-    st.header(stockname)
-
-    df.drop(["Dividends", "Stock Splits"], axis=1, inplace=True)
-    st.write(df.tail())
-
-    # Initialize session state for date selection
-    if "selected_date" not in st.session_state:
-        st.session_state.selected_date = datetime.date.today()
-
-    with st.container():
-        col1, col2 = st.columns([1, 1])
-
-        with col1:
-            # Date selection with session state
-            st.session_state.selected_date = st.date_input("Select Date", st.session_state.selected_date)
-            st.write("Selected Date:", st.session_state.selected_date)
-
-            # Charts
-            st.line_chart(df, x="Date", y="Close", color="#ff00f0")
-            st.area_chart(df, x="Date", y="Volume", color="#ff00ff")
-            st.divider()
-
-        with col2:
-            st.bar_chart(df, x="Date", y="Volume")
-            st.scatter_chart(df, x="Date", y="High", color=["#FF0000"])
-            fig = px.bar(df, x='Date', y='Volume')
-            st.plotly_chart(fig)
+        st.write(data)
